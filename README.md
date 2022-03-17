c3316ea2200af1cfc5b7eb106fd718815f6156b9
luisgar289 <1720110205@utectulancingo.edu.mx>
2022-03-17 18:41:15 +0000

    v0.2

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 ---
 ### Versiones:
 
-1. v0.1 - Primera version
+1. v0.1 - Primera versión.
 * Contiene la estrucutra base de una aplicaciónlicacion web con MVC
+
+2. v0.2 - Versión funcional.
+* Hola mundo MVC con style mediante CSS.
--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-#find python hello world. 
+import web
+
+urls = (
+    '/', 'mvc.controllers.public.index.Index' #se indica la ruta donde esta el archivo .py
+)
+app = web.application(urls, globals()) 
+
+if __name__ == "__main__":
+    app.run()
